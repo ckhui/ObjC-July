e097b3e247cd6c441875a484a79d3146a1ebfc79
CKHui <ckhanghui@gmail.com>
2017-09-20 05:18:06 +0800

    add customeTableViewCell

---
+++ Obj-C-Assessment2/ProdcutTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  ProdcutTableViewCell.h
+//  Obj-C-Assessment2
+//
+//  Created by CKH on 20/09/2017.
+//  Copyright © 2017 CKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProdcutTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+
+
+@end
---
+++ Obj-C-Assessment2/ProdcutTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ProdcutTableViewCell.m
+//  Obj-C-Assessment2
+//
+//  Created by CKH on 20/09/2017.
+//  Copyright © 2017 CKH. All rights reserved.
+//
+
+#import "ProdcutTableViewCell.h"
+
+@implementation ProdcutTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
