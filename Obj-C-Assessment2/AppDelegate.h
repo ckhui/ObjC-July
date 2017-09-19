12c05d88606bb096b5a39945bdc4065adf9dd8ca
CKHui <ckhanghui@gmail.com>
2017-09-20 03:21:24 +0800

    Initial Commit

---
+++ Obj-C-Assessment2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Obj-C-Assessment2
+//
+//  Created by CKH on 20/09/2017.
+//  Copyright © 2017 CKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Obj-C-Assessment2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Obj-C-Assessment2
+//
+//  Created by CKH on 20/09/2017.
+//  Copyright © 2017 CKH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
